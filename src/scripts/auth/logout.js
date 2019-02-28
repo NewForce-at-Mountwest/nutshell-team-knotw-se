16ac5f40f4ed2784b534095250f3a7d6242fe980
Connor FitzGerald <connorreecefitzgerald@gmail.com>
2019-02-28 15:26:32 -0500

    fixed an oopsie

--- src/scripts/auth/logout.js
+++ src/scripts/auth/logout.js
@@ -4,7 +4,7 @@ import clicks from "./clicks"
 const handleLogout = () => {
     sessionStorage.removeItem("userId")
     console.log("Hello")
-    document.querySelector("#new-task-container").innerHTML = ""
+    document.querySelector("#task").innerHTML = ""
     document.querySelector("#print-task").innerHTML = ""
     document.querySelector("#jh-home").innerHTML = authForm.home()
 // Attempting to make it where after you logout you can log back in / thought importing all the clicks would work but it doesn't look like it .:)
